5161c23df3cdb1bec64d514e8b828b2ab7894d77
rayancastro <rayancdc@gmail.com>
2019-02-10 20:02:04 -0200

    Add admin dashboard and require admin to create posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :require_admin
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :ip])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
+
+  def require_admin
+    unless current_user && current_user.admin
+      flash[:notice] = "Permissão negada. Favor logar como administrador."
+      redirect_to(root_path)
+    end
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :require_admin, except: [:admin]
 
   def home
   end
+
+  def admin
+  end
+
 end
