00d0cfa12e8761b1bd6149a8b3f5c456ceb8753a
rayancastro <rayancdc@gmail.com>
2019-02-10 23:03:55 -0200

    Fix required admin for devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
-  skip_before_action :require_admin, except: [:index, :show]
+  skip_before_action :require_admin, only: [:index, :show]
   before_action :set_post, only: [:show, :edit, :update, :delete]
 
   def index
