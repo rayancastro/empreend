8256b0728d65221c8bbe57e2ed7da3af73ff3f03
rayancastro <rayancdc@gmail.com>
2019-02-11 23:00:37 -0200

    Add banner to post and validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true
+  validates :display_url, presence: true, uniqueness: true
 
   mount_uploader :primary_photo, PhotoUploader
 end
