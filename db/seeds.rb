6248a3cdfb3461c2e7f0735928b8b99fba7553cc
rayancastro <rayancdc@gmail.com>
2019-02-15 08:49:27 -0200

    Fix post show and improve admin bar

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+200.times do |i|
+  Lead.create!(nome: "Hamaji-#{i}", sobrenome: "Mauricio", email: "mhomma-#{i}@gmail.com", ip: "#{i}")
+end
