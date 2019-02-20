3828cc05cd69892c3896de4ffedc6e8d1c35efd0
rayancastro <rayancdc@gmail.com>
2019-02-20 12:22:20 -0300

    Add navbar links and button.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,7 @@
 import "bootstrap";
 import { setStickyFooter } from '../components/footer';
-
+import { offsetAnchor } from '../components/offsetanchor';
 console.log('Webpack says hi.');
 
 // setStickyFooter();
+
